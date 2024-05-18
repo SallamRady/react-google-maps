9ef31ef8a534b716d9a7e8d5272e54b231d553d0
Sallam Rady <sallamrady@gmail.com>
2024-05-18 14:19:20 +0300

    react google map api have double features (Map + Satellite)

--- src/components/GooogleMap.tsx
+++ src/components/GooogleMap.tsx
@@ -1,10 +1,4 @@
-import {
-  GoogleMap,
-  InfoWindow,
-  Marker,
-  Polygon,
-  useJsApiLoader,
-} from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import { useState } from "react";
 
 // * Define some helper variables
@@ -12,6 +6,7 @@ const containerStyle = {
   width: "400px",
   height: "400px",
 };
+
 const center = {
   lat: 30.0444,
   lng: 31.2357,
@@ -21,7 +16,6 @@ const elShikhZaid = {
   lat: 30.64649,
   lng: 31.900129,
 };
-const polygonPaths = [center, giza, elShikhZaid];
 
 export default function GooogleMap() {
   // TODO::declare and define component state and variables
@@ -30,13 +24,6 @@ export default function GooogleMap() {
     id: "AIzaSyCLMkS3uH2eW8Fn7a36lKama2jJW9KFFhc",
     googleMapsApiKey: "AIzaSyCLMkS3uH2eW8Fn7a36lKama2jJW9KFFhc",
   });
-  // * Handle click on map
-  const handleMapClick = (event: google.maps.MapMouseEvent) => {
-    const { latLng } = event;
-    const lat = latLng?.lat();
-    const lng = latLng?.lng();
-    console.log("Map clicked:", lat, lng);
-  };
 
   return isLoaded ? (
     <GoogleMap
@@ -45,30 +32,7 @@ export default function GooogleMap() {
       zoom={10}
       //   onLoad={onLoad}
       //   onUnmount={onUnmount}
-    //   onClick={handleMapClick}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <Marker position={center} />
-      <Marker position={giza} />
-      {/* custom maker */}
-      {/* polygon */}
-      <Polygon
-        paths={polygonPaths}
-        options={{
-          strokeColor: "#FF0000",
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: "#FF0000",
-          fillOpacity: 0.35,
-        }}
-      />
-      {/* To Show information on mab like title */}
-      <InfoWindow position={center}>
-        <div>
-          <h3>Cairo City</h3>
-        </div>
-      </InfoWindow>
-    </GoogleMap>
+    ></GoogleMap>
   ) : (
     <>Gooogle Mapppppp</>
   );
